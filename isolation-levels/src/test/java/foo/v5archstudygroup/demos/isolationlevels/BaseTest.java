68eddb03f584016b9f9287a182c2c464003d64a5
Petter Holmström <petter@vaadin.com>
2022-06-23 00:09:49 +0300

    Clean up tests

---
+++ isolation-levels/src/test/java/foo/v5archstudygroup/demos/isolationlevels/BaseTest.java
@@ -0,0 +1,113 @@
+package foo.v5archstudygroup.demos.isolationlevels;
+
+import foo.v5archstudygroup.demos.isolationlevels.entity.Unit;
+import foo.v5archstudygroup.demos.isolationlevels.entity.UnitRepository;
+import foo.v5archstudygroup.demos.isolationlevels.entity.UnitStatus;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public abstract class BaseTest {
+
+    private final Semaphore readSemaphore = new Semaphore(0);
+    private final Semaphore writeSemaphore = new Semaphore(0);
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final TransactionTemplate transactionTemplate;
+    private final EntityManager entityManager;
+    private final UnitRepository unitRepository;
+
+    BaseTest(PlatformTransactionManager transactionManager, EntityManager entityManager,
+             UnitRepository unitRepository) {
+        this.entityManager = entityManager;
+        this.unitRepository = unitRepository;
+
+        transactionTemplate = new TransactionTemplate(transactionManager);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        transactionTemplate.setIsolationLevel(getIsolationLevel());
+    }
+
+    protected abstract int getIsolationLevel();
+
+    @BeforeEach
+    public void setUp() {
+        for (int i = 10; i <= 99; ++i) {
+            unitRepository.saveAndFlush(new Unit("RVS" + i + "1"));
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+        unitRepository.deleteAllInBatch();
+    }
+
+    protected void runInTransaction(Runnable job) {
+        log.info("Transaction begins");
+        transactionTemplate.executeWithoutResult(tx -> job.run());
+        log.info("Transaction committed");
+    }
+
+    protected void allowReading() {
+        readSemaphore.release();
+    }
+
+    protected void allowWriting() {
+        writeSemaphore.release();
+    }
+
+    protected void awaitReadPermission() {
+        try {
+            if (!readSemaphore.tryAcquire(1, TimeUnit.SECONDS)) {
+                Assertions.fail("Could not acquire read permission");
+            }
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    protected void awaitWritePermission() {
+        try {
+            if (!writeSemaphore.tryAcquire(1, TimeUnit.SECONDS)) {
+                Assertions.fail("Could not acquire write permission");
+            }
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    protected Unit getByCallSign(String callSign) {
+        var unit = unitRepository.getByCallSign(callSign);
+        // To always force the next query to the database instead of using the entity manager cache.
+        entityManager.detach(unit);
+        return unit;
+    }
+
+    protected void saveAndFlush(Unit unit) {
+        unitRepository.saveAndFlush(unit);
+        // To always force the next query to the database instead of using the entity manager cache.
+        entityManager.detach(unit);
+    }
+
+    protected void deleteAndFlush(Unit unit) {
+        unitRepository.delete(unit);
+        unitRepository.flush();
+    }
+
+    protected List<Unit> findByStatus(UnitStatus status) {
+        var result = unitRepository.findByStatusOrderByCallSign(status);
+        // To always force the next query to the database instead of using the entity manager cache.
+        //entityManager.clear();
+        return result;
+    }
+}
--- isolation-levels/src/test/java/foo/v5archstudygroup/demos/isolationlevels/ReadCommitted.java
+++ isolation-levels/src/test/java/foo/v5archstudygroup/demos/isolationlevels/ReadCommitted.java
@@ -1,165 +1,100 @@
 package foo.v5archstudygroup.demos.isolationlevels;
 
-import foo.v5archstudygroup.demos.isolationlevels.entity.Unit;
 import foo.v5archstudygroup.demos.isolationlevels.entity.UnitRepository;
 import foo.v5archstudygroup.demos.isolationlevels.entity.UnitStatus;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
 import java.time.Clock;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.CompletableFuture;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-public class ReadCommitted {
-
-    private static final Logger LOG = LoggerFactory.getLogger(ReadCommitted.class);
+public class ReadCommitted extends BaseTest {
 
     @Autowired
-    UnitRepository unitRepository;
-
-    @Autowired
-    PlatformTransactionManager transactionManager;
-
-    @Autowired
-    EntityManager entityManager;
-
-    @Autowired
-    Clock clock;
-
-    TransactionTemplate txTemplate;
-
-    @BeforeEach
-    public void setUp() {
-        txTemplate = new TransactionTemplate(transactionManager);
-        txTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
-
-        for (int i = 10; i < 99; ++i) {
-            unitRepository.saveAndFlush(new Unit("RVS" + i + "1"));
-        }
-    }
-
-    @AfterEach
-    public void tearDown() {
-        unitRepository.deleteAll();
+    ReadCommitted(PlatformTransactionManager transactionManager, EntityManager entityManager, UnitRepository unitRepository) {
+        super(transactionManager, entityManager, unitRepository);
     }
 
     @Test
-    public void transactions_cannot_see_each_others_changes_before_committing() throws Exception {
-        // These semaphores are just used to make sure the threads do what they are expected to, in the correct
-        // order.
-        var writeSemaphore = new Semaphore(0);
-        var readSemaphore = new Semaphore(0);
-        var writeThread = new Thread(() -> {
-            LOG.info("Transaction begins");
-            txTemplate.executeWithoutResult(tx -> {
-                try {
-                    writeSemaphore.acquire();
-                    {
-                        var unit = unitRepository.getByCallSign("RVS911");
-                        unit.updateStatus(clock, UnitStatus.EN_ROUTE);
-                        unitRepository.saveAndFlush(unit);
-                        LOG.info("Changed status to EN_ROUTE");
-                    }
-                    readSemaphore.release();
+    public void transactions_cannot_see_each_others_changes_before_committing() {
+        var writeThread = CompletableFuture.runAsync(() -> {
+            runInTransaction(() -> {
+                awaitWritePermission();
+                {
+                    var unit = getByCallSign("RVS911");
+                    unit.updateStatus(Clock.systemDefaultZone(), UnitStatus.EN_ROUTE);
+                    saveAndFlush(unit);
+                    log.info("Changed status to EN_ROUTE");
+                }
+                allowReading();
 
-                    writeSemaphore.acquire();
-                    {
-                        var unit = unitRepository.getByCallSign("RVS911");
-                        unit.updateStatus(clock, UnitStatus.ON_SCENE);
-                        unitRepository.saveAndFlush(unit);
-                        LOG.info("Changed status to ON_SCENE");
-                    }
-                } catch (InterruptedException ignoreIt) {
+                awaitWritePermission();
+                {
+                    var unit = getByCallSign("RVS911");
+                    unit.updateStatus(Clock.systemDefaultZone(), UnitStatus.ON_SCENE);
+                    saveAndFlush(unit);
+                    log.info("Changed status to ON_SCENE");
                 }
             });
-            LOG.info("Transaction committed");
         });
-        var readThread = new Thread(() -> {
-            LOG.info("Transaction begins");
-            txTemplate.executeWithoutResult(tx -> {
-                try {
-                    LOG.info("Checking initial status, assuming UNKNOWN");
-                    assertThat(unitRepository.getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.UNKNOWN);
-                    writeSemaphore.release();
+        var readThread = CompletableFuture.runAsync(() -> {
+            runInTransaction(() -> {
+                log.info("Checking initial status, assuming UNKNOWN");
+                assertThat(getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.UNKNOWN);
+                allowWriting();
 
-                    readSemaphore.acquire();
-                    LOG.info("Checking status, still assuming UNKNOWN");
-                    entityManager.clear(); // Without this, we will get a cached version when we run the query below
-                    assertThat(unitRepository.getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.UNKNOWN);
-                    writeSemaphore.release();
-                } catch (InterruptedException ignoreIt) {
-                }
+                awaitReadPermission();
+                log.info("Checking status, still assuming UNKNOWN");
+                assertThat(getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.UNKNOWN);
+                allowWriting();
             });
-            LOG.info("Transaction committed");
         });
 
-        writeThread.start();
-        readThread.start();
+        readThread.join();
+        writeThread.join();
+
+        assertThat(getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.ON_SCENE);
+    }
+
+    @Test
+    public void the_same_query_inside_the_same_transaction_returns_different_results_if_another_transaction_commits_changes_in_between() {
+        var writeThread = CompletableFuture.runAsync(() -> {
+            runInTransaction(() -> {
+                awaitWritePermission();
+                var unit = getByCallSign("RVS911");
+                unit.updateStatus(Clock.systemDefaultZone(), UnitStatus.EN_ROUTE);
+                saveAndFlush(unit);
+                log.info("Changed status to EN_ROUTE");
+            });
+            allowReading();
+        });
+        var readThread = CompletableFuture.runAsync(() -> {
+            runInTransaction(() -> {
+                log.info("Checking initial status, assuming UNKNOWN");
+                assertThat(getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.UNKNOWN);
+                allowWriting();
+
+                awaitReadPermission();
+                log.info("Checking status, assuming EN_ROUTE");
+                assertThat(getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.EN_ROUTE);
+            });
+        });
 
         writeThread.join();
         readThread.join();
 
-        assertThat(unitRepository.getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.ON_SCENE);
+        assertThat(getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.EN_ROUTE);
     }
 
-    @Test
-    public void the_same_query_inside_the_same_transaction_returns_different_results_if_another_transaction_commits_changes_in_between() throws Exception {
-        // These semaphores are just used to make sure the threads do what they are expected to, in the correct
-        // order.
-        var writeSemaphore = new Semaphore(0);
-        var readSemaphore = new Semaphore(0);
-        var writeThread = new Thread(() -> {
-            LOG.info("Transaction begins");
-            txTemplate.executeWithoutResult(tx -> {
-                try {
-                    writeSemaphore.acquire();
-                    {
-                        var unit = unitRepository.getByCallSign("RVS911");
-                        unit.updateStatus(clock, UnitStatus.EN_ROUTE);
-                        unitRepository.saveAndFlush(unit);
-                        LOG.info("Changed status to EN_ROUTE");
-                    }
-                } catch (InterruptedException ignoreIt) {
-                }
-            });
-            LOG.info("Transaction committed");
-            readSemaphore.release();
-        });
-        var readThread = new Thread(() -> {
-            LOG.info("Transaction begins");
-            txTemplate.executeWithoutResult(tx -> {
-                try {
-                    LOG.info("Checking initial status, assuming UNKNOWN");
-                    assertThat(unitRepository.getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.UNKNOWN);
-                    writeSemaphore.release();
-
-                    readSemaphore.acquire();
-                    LOG.info("Checking status, assuming EN_ROUTE");
-                    entityManager.clear(); // Without this, we will get a cached version when we run the query below
-                    assertThat(unitRepository.getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.EN_ROUTE);
-                } catch (InterruptedException ignoreIt) {
-                }
-            });
-            LOG.info("Transaction committed");
-        });
-
-        writeThread.start();
-        readThread.start();
-
-        writeThread.join();
-        readThread.join();
-
-        assertThat(unitRepository.getByCallSign("RVS911").getStatus()).isEqualTo(UnitStatus.EN_ROUTE);
+    @Override
+    protected int getIsolationLevel() {
+        return TransactionDefinition.ISOLATION_READ_COMMITTED;
     }
 }
